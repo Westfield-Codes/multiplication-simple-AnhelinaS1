458cbdc45ff869dce7d9ab47b26f89a8fcf419f8
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-03-28 10:53:57 -0400

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,18 @@
+function main(){
+let questions= 5;
+let right = askQuestions(questions);
+if (right == questions) alert("perfect!")
+else alert("You got" + right + "out of" + questions);
+}
+
+function askQuestions(questions){
+    let question = 1;
+    while (question <= questions){
+        right += askQuestions(question);
+        question ++;
+    }
+return right;
+}
+
+function askQuestion(question);
+let a = Math.floor(Math.random()*6)+1;
