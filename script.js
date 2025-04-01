c96bbf6c25285090f89e02511df3ae1a120f3e4e
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-04-01 14:49:38 +0000

    multiplication done

--- script.js
+++ script.js
@@ -6,16 +6,27 @@ else alert("You got" + right + "out of" + questions);
 }
 
 function askQuestions(questions){
+    let right = 0;
     let question = 1;
     while (question <= questions){
-        right += askQuestions(question);
+       right += askQuestion(question);
         question ++;
     }
 return right;
 }
 
-function askQuestion(question);
+function askQuestion(question){
 let a = Math.floor(Math.random()*6)+3;
 let b = Math.floor(Math.random()*6)+3;
 let product = a*b;
-
+let equation = "Question" + question + ": " + a + "*" + b + "=?";
+let answer = prompt(equation);
+if (answer == product){
+     alert("correct!");
+    return true;
+}
+else {
+    alert("Incorrect!");
+    return false;
+    }
+}
